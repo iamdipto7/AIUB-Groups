95a466e688e6aee4f7b20bc19533d1a3956d9367
K.M. Mahbubuzzaman <iamdipto7@google.com>
2018-01-12 13:37:36 +0600

    reconfigure

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,13 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function (formidable,Group,aws) {
   return {
     SetRouting: function (router) {
       router.get('/dashboard',this.adminPage);
 
-      router.post('/uploadFile', aws.Upload.any());
+      router.post('/uploadFile', this.uploadFile);
       router.post('/dashboard',this.adminPostPage);
     },
     adminPage: function (req,res) {
@@ -23,17 +25,17 @@ module.exports = function (formidable,Group,aws) {
     },
     uploadFile: function (req,res) {
       const form = new formidable.IncomingForm();
-      //form.uploadDir = path.join(__dirname,'../public/uploads');
+      form.uploadDir = path.join(__dirname,'../public/uploads');
 
       form.on('file',(field,file)=>{
-        // fs.rename(file.path,path.join(form.uploadDir,file.name),(err)=>{
-        //   if (err) {
-        //     throw err;
-        //   }
-        //   else {
-        //     console.log('File rename successfully');
-        //   }
-        // })
+        fs.rename(file.path,path.join(form.uploadDir,file.name),(err)=>{
+          if (err) {
+            throw err;
+          }
+          else {
+            console.log('File rename successfully');
+          }
+        })
       });
       form.on('error',(err)=>{
         console.log(err);
